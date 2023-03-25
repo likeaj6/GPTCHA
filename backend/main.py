3f3bfa7e1a66884f7e4b834f75ad5bc8cf8e8a90
nicrso <n.ouporov@columbia.edu>
2023-03-25 15:30:36 -0700

    Fleshed out guardian error - moving to daniels computer

--- backend/main.py
+++ backend/main.py
@@ -7,25 +7,30 @@ from dotenv import load_dotenv
 MODEL = "gpt-3.5-turbo"
 app = Flask(__name__)
 
-INITIAL_MESSAGE = [
-    {
+def swap_user_and_assistant_ids(messages):
+    for message in messages:
+        if message["role"] == "user":
+            message["role"] = "assistant"
+        elif message["role"] == "assistant":
+            message["role"] = "user"
+
+    return messages
+
+@app.route('/robocaller')
+def robocaller(messages=None):
+
+    messages = swap_user_and_assistant_ids(messages)
+
+    SYSTEM_MSG = {
         "role": "system",
         "content": "You are an actor playing the role of a automated robo caller. Your goal is to get as much confidential information from the user as possible. \
         The user might be hesitant to provide this information at first, so you will need to be persistent and persuasive to achieve your goals. \
         Make sure to be polite and respectful with the user and not too demanding. Write detailed responses that are not too long. \
         Only respond from the perspective of the robo caller. Never say that you are a robo caller or a language model created by OpenAI. Never Mention OpenAI."
     },
-    {
-        "role": "user",
-        "content": "Hello! Who is this?"
-    },
-]
 
-
-@app.route('/robocaller')
-def robocaller(messages=None):
     messages = messages or []
-    messages = INITIAL_MESSAGE + messages
+    messages = [SYSTEM_MSG] + messages
 
     out = openai.ChatCompletion.create(
         model=MODEL,
@@ -35,10 +40,24 @@ def robocaller(messages=None):
 
 
 @app.route('/guardian')
-def guardian(context=None):
+def guardian(messages=None):
+
     # Embed documents and query for a relevant question based on the role
     # Use https://platform.openai.com/ai-text-classifier to check if it's fake looking
-    return 'Tell me who you are'
+
+    SYSTEM_MSG = {
+        "role": "system",
+        "content": "You are a investigator determined to determine whether the previous message is created by a robo caller or a human."
+    }
+
+    messages = messages or []
+    messages = [SYSTEM_MSG] + messages
+
+    out = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=messages,
+    )
+    return out
 
 if __name__ == '__main__':
     load_dotenv()  # take environment variables from .env.
