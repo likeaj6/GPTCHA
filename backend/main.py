6f56b8eec875044415f076e16b08a48c82e7c323
Daniel <13357734+thedch@users.noreply.github.com>
2023-03-25 15:06:21 -0700

    Fixup and make it nicer

--- backend/main.py
+++ backend/main.py
@@ -7,35 +7,37 @@ from dotenv import load_dotenv
 MODEL = "gpt-3.5-turbo"
 app = Flask(__name__)
 
-
-@app.route('/robocaller')
-def robocaller(messages=None):
-    messages = messages or []
-    initial_message = {
-        "role": "user",
-        "content": "Hello! Who is this?"
-    }
-    context = {
+INITIAL_MESSAGE = [
+    {
         "role": "system",
         "content": "You are an actor playing the role of a automated robo caller. Your goal is to get as much confidential information from the user as possible. \
         The user might be hesitant to provide this information at first, so you will need to be persistent and persuasive to achieve your goals. \
         Make sure to be polite and respectful with the user and not too demanding. Write detailed responses that are not too long. \
         Only respond from the perspective of the robo caller. Never say that you are a robo caller or a language model created by OpenAI. Never Mention OpenAI."
-    }
-    if len(messages) != 0:
-        messages = [context] + messages
-    else:
-        messages =  [context] + [initial_message]
+    },
+    {
+        "role": "user",
+        "content": "Hello! Who is this?"
+    },
+]
+
+
+@app.route('/robocaller')
+def robocaller(messages=None):
+    messages = messages or []
+    messages = INITIAL_MESSAGE + messages
+
     out = openai.ChatCompletion.create(
         model=MODEL,
         messages=messages,
     )
-    print(out)
     return out
 
 
 @app.route('/guardian')
 def guardian(context=None):
+    # Embed documents and query for a relevant question based on the role
+    # Use https://platform.openai.com/ai-text-classifier to check if it's fake looking
     return 'Tell me who you are'
 
 if __name__ == '__main__':
